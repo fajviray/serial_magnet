6458bbd3084313eb07e4923e8fd471ad42fc25ba
Yana <fajviray@gmail.com>
2017-01-18 22:03:57 +0800

    fix season list

--- lib/serial_magnet/utils.rb
+++ lib/serial_magnet/utils.rb
@@ -4,6 +4,17 @@ module SerialMagnet
       response = SerialMagnet::Request.new(self, :get, "#{path}/#{id}").perform
       response[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
     end
+
+    def perform_get_all type, path, key=path
+      response = SerialMagnet::Request.new(self, :get, "#{path}").perform
+      response[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
+      count = (response["meta"]["total"]/10).ceil
+      (1..count).each do |x|
+        result = SerialMagnet::Request.new(self, :get, "#{path}?page=#{x+1}").perform
+        result[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
+        response[key] = response[key] + result[key]
+      end
+    end
   end
 end
 
