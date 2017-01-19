a19bca6ebb5538488e70e6f027d04c60e0303357
Yana <fajviray@gmail.com>
2017-01-19 15:24:16 +0800

    fix response data

--- lib/serial_magnet/utils.rb
+++ lib/serial_magnet/utils.rb
@@ -7,15 +7,12 @@ module SerialMagnet
 
     def perform_get_all type, path, key=path
       response = SerialMagnet::Request.new(self, :get, "#{path}").perform
-      response[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
       count = (response["meta"]["total"]/10).ceil
       (1..count).each do |x|
         result = SerialMagnet::Request.new(self, :get, "#{path}?page=#{x+1}").perform
-        result[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
         response[key] = response[key] + result[key]
       end
-      return response[key]
+      response[key].each_with_index.map { |object, index| SerialMagnet::Object.new({ type => object.merge(number: index + 1) }) }
     end
   end
 end
-
